ae4aad91568a8732e454ba44bf92abaa313c7094
Onelight <150380264+Onelight123@users.noreply.github.com>
2025-09-18 22:34:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
-// Welcome to the configuration file for Pandy.
-// https://github.com/EgooDev/PandyAI
-// To get our API keu, please visit https://openrouter.ai/settings/keys and create an account.
-// Replace the API_KEY below with your own key.
-// Example: const API_KEY = "API-FAKEAPIEXAMPLEFORPANDY";
 
-const API_KEY = "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";
+const API_KEY = "";
+
 
