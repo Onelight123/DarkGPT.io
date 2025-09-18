8b807cb78f3073335666b28647875b4ae42fcddb
Onelight <150380264+Onelight123@users.noreply.github.com>
2025-09-18 21:58:00 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,5 @@
 // To get our API keu, please visit https://openrouter.ai/settings/keys and create an account.
 // Replace the API_KEY below with your own key.
 // Example: const API_KEY = "API-FAKEAPIEXAMPLEFORPANDY";
-const API_KEY = "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";
+
+const API_KEY = "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";
