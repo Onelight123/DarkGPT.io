8a19be25b11c1e602a6fb2225971209d59258612
Onelight <150380264+Onelight123@users.noreply.github.com>
2025-09-18 21:45:44 +0200

    Add files via upload
    Add file

---
+++ config.js
@@ -0,0 +1,6 @@
+// Welcome to the configuration file for Pandy.
+// https://github.com/EgooDev/PandyAI
+// To get our API keu, please visit https://openrouter.ai/settings/keys and create an account.
+// Replace the API_KEY below with your own key.
+// Example: const API_KEY = "API-FAKEAPIEXAMPLEFORPANDY";
+const API_KEY = "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";
